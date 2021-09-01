bf7f60b95bcb118684f60c0b18a8baf2fd928c09
ErikBurdett <erikaburdet@gmail.com>
2021-09-01 17:31:49 -0500

    added tests for POST PUT DELETE GET

--- src/components/__test__/APIService.test.js
+++ src/components/__test__/APIService.test.js
@@ -1,7 +1,10 @@
 import React from "react";
-import {useEffect} from '../../App'
-import "@testing-library/jest-dom/extend-expect"
+import {useEffect} from '../../App';
+import insertedArticle from'../Form';
+import deleteArticle from '../ArticleList';
+import "@testing-library/jest-dom/extend-expect";
 import fetchMock from "jest-fetch-mock";
+import APIService, {UpdateArticle, InsertedArticle, DeleteArticle } from '../APIService';
 
 fetchMock.enableMocks();
 
@@ -10,9 +13,43 @@ beforeEach(()=>{
 
 });
 
+// const testArticle = [{
+//     title: 'test title',
+//     body: 'test'
+// }]
+
+// App specific get function for rendering Articles on Page
 test("Succesfully GETs all Articles", () => {
     fetch.mockResponse(() => useEffect().then(res => ({ body: 'ok' })))});
+test("Succesfully POSTs InsertedArticle", () => {
+    fetch.mockResponseOnce(()=> insertedArticle().then(res =>({body:{title: 'testing post test', body:'testing post test' }})))
+});
+test("Succesfully PUTSs InsertedArticle", () => {
+    fetch.mockResponseOnce(()=> upatedArticle().then(res =>({body:{title: 'testing post test', body:'testing post test' }})))
+});
+test("Succesfully Deletes article", () => {
+    fetch.mockResponseOnce(()=> deleteArticle().then(res =>({body:'ok' })))
+});
 
+
+
+// test("Succesfully POSTs InsertedArticle", () => {
+//     fetch.mockResponseOnce(JSON.stringify([{title:'testing test POST request', body: '1'}]));
+//     const onReponse = jest.fn();
+//     const onError = jest.fn();
+
+//     return APIService.InsertedArticle()
+//         .then(onReponse)
+//         .catch(onError)
+//         .finally(()=>{
+//             expect(onReponse).toHaveBeenCalled();
+//             expect(onError).not.toHaveBeenCalled();
+
+//             expect(onResponse.mock.calls[0][0][0]).toEqual({article});
+
+//         })
+
+//     })
 
 // test("returns null when exception", () => {
 //     fetch.mockReject(() => Promise.reject("API is down"));
